6a5a4c8b73da3307fcc30b31374f763bb044255a
psittacine <psittacine714@gmail.com>
2018-10-15 22:06:16 -0500

    Setup SearchPage

---
+++ src/components/pages/SearchPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class SearchPage extends React.Component {
+    render() {
+        return (
+            <p>search</p>
+        );
+    }
+}
+
+
+
+
+export default SearchPage
