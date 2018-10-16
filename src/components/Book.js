6bdc0927b56dc9af2e83eac9c1f2eb48f19d0320
psittacine <psittacine714@gmail.com>
2018-10-16 14:40:55 -0500

    Setup Book

---
+++ src/components/Book.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+class Book extends React.Component {
+  render() {
+    return (
+        // content
+    );
+  }
+}
+
+export default Book;
