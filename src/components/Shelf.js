2cc742e15df4a73122ae4685d93efe33158ee14d
psittacine <psittacine714@gmail.com>
2018-10-16 14:07:46 -0500

    Setup Shelf

---
+++ src/components/Shelf.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+class Shelf extends React.Component {
+  render() {
+    return (
+        // content
+    );
+  }
+}
+
+export default Shelf;
