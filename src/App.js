2846a03912fbd77532c4fc0020eca512ce5774db
psittacine <psittacine714@gmail.com>
2020-08-14 23:12:27 -0500

    [React ND 2020] Refactor Class Components into Stateless Functional Components

--- src/App.js
+++ src/App.js
@@ -4,15 +4,11 @@ import { Route } from "react-router-dom";
 import MainPage from "./components/MainPage";
 import SearchPage from "./components/SearchPage";
 
-class BooksApp extends React.Component {
-	render() {
-		return (
-			<div>
-				<Route exact path="/" component={MainPage} />
-				<Route exact path="/search" component={SearchPage} />
-			</div>
-		);
-	}
-}
+const BooksApp = () => (
+	<div>
+		<Route exact path="/" component={MainPage} />
+		<Route exact path="/search" component={SearchPage} />
+	</div>
+)
 
-export default BooksApp;
+export default BooksApp
--- src/components/Book.js
+++ src/components/Book.js
@@ -1,49 +1,45 @@
 import React from "react";
 
-class Book extends React.Component {
-	render() {
-		return (
-			<li>
-				<div className="book">
-					<div className="book-top">
-						<div
-							className="book-cover"
-							style={{
-								width: 128,
-								height: 193,
-								backgroundImage: `url("${(this.props.book.imageLinks &&
-									this.props.book.imageLinks.thumbnail) ||
-									""}")`
-							}}
-						/>
-						<div className="book-shelf-changer">
-							<select
-								value={this.props.book.shelf || "none"}
-								onChange={event => {
-									this.props.updateBook(this.props.book, event.target.value);
-								}}
-							>
-								<option value="move" disabled>
-									Move to...
-								</option>
-								<option value="currentlyReading">Currently Reading</option>
-								<option value="wantToRead">Want to Read</option>
-								<option value="read">Read</option>
-								<option value="none">None</option>
-							</select>
-						</div>
-					</div>
-					<div className="book-title">
-						{this.props.book.title || "Untitled"}
-					</div>
-					<div className="book-authors">
-						{(this.props.book.authors && this.props.book.authors[0]) ||
-							"Anonymous"}
-					</div>
+const Book = (props) => (
+	<li>
+		<div className="book">
+			<div className="book-top">
+				<div
+					className="book-cover"
+					style={{
+						width: 128,
+						height: 193,
+						backgroundImage: `url("${(props.book.imageLinks &&
+							props.book.imageLinks.thumbnail) ||
+							""}")`
+					}}
+				/>
+				<div className="book-shelf-changer">
+					<select
+						value={props.book.shelf || "none"}
+						onChange={event => {
+							props.updateBook(props.book, event.target.value);
+						}}
+					>
+						<option value="move" disabled>
+							Move to...
+						</option>
+						<option value="currentlyReading">Currently Reading</option>
+						<option value="wantToRead">Want to Read</option>
+						<option value="read">Read</option>
+						<option value="none">None</option>
+					</select>
 				</div>
-			</li>
-		);
-	}
-}
+			</div>
+			<div className="book-title">
+				{props.book.title || "Untitled"}
+			</div>
+			<div className="book-authors">
+				{(props.book.authors && props.book.authors[0]) ||
+					"Anonymous"}
+			</div>
+		</div>
+	</li>
+)
 
-export default Book;
+export default Book
